7a5041db2d7bcd5b433072a67425fc34f5fb62c9
Nikita Krochak <krochak@in4security.com>
2022-09-20 14:39:36 +1000

    Create root command

---
+++ cmd/root.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "go-project-lvl1",
+	Short: "A pack of simple math games.",
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/InfluxOW/go-project-lvl1/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
